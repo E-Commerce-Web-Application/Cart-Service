30b4ec31df9d573f93a847c72cf9bc9ec70fb882
Dinuka Lakmal <dinukalakmalwich7084@gmail.com>
2026-02-28 17:54:24 +0530

    cart routes updated

--- backend/src/routes/cartRoutes.js
+++ backend/src/routes/cartRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/authMiddleware");
 const cartController = require("../controllers/cartController");
 
-router.get("/", auth, cartController.getCart);
-router.post("/add", auth, cartController.addItem);
-router.delete("/remove/:productId", auth, cartController.removeItem);
-router.delete("/clear", auth, cartController.clearCart);
+router.get("/", cartController.getCart);
+router.post("/add", cartController.addItem);
+router.delete("/remove/:productId", cartController.removeItem);
+router.delete("/clear", cartController.clearCart);
 
 module.exports = router;
